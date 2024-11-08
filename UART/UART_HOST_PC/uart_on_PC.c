4c5b62fdfc647734c82d664866f629166dcafa5e
Ho Cong Hieu <21200289@student.hcmus.edu.vn>
2024-11-08 13:18:53 +0700

    first commit

---
+++ UART/UART_HOST_PC/uart_on_PC.c
@@ -0,0 +1,148 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+
+#define BUFFER_SIZE 256
+
+HANDLE open_port(const char* device, unsigned long baud_rate, unsigned char bit_size, unsigned char parity) {
+    HANDLE port = CreateFileA(device, GENERIC_READ | GENERIC_WRITE, 0, NULL,
+        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (port == INVALID_HANDLE_VALUE)
+    {
+        return INVALID_HANDLE_VALUE;
+    }
+
+    // Flush away any bytes previously read or written.
+    BOOL success = FlushFileBuffers(port);
+    if (!success)
+    {
+        printf("Failed to flush serial port");
+        CloseHandle(port);
+        return INVALID_HANDLE_VALUE;
+    }
+
+    // Configure read and write operations to time out after 100 ms.
+    COMMTIMEOUTS timeouts = { 0 };
+    timeouts.ReadIntervalTimeout = 0;
+    timeouts.ReadTotalTimeoutConstant = 100;
+    timeouts.ReadTotalTimeoutMultiplier = 0;
+    timeouts.WriteTotalTimeoutConstant = 100;
+    timeouts.WriteTotalTimeoutMultiplier = 0;
+
+    success = SetCommTimeouts(port, &timeouts);
+    if (!success)
+    {
+        printf("Failed to set serial timeouts");
+        CloseHandle(port);
+        return INVALID_HANDLE_VALUE;
+    }
+
+    // Set the baud rate and other options.
+    DCB dcb = { 0 };
+    dcb.DCBlength = sizeof(DCB);
+    dcb.BaudRate = baud_rate;
+    dcb.ByteSize = bit_size;
+    dcb.Parity = parity;
+    dcb.StopBits = ONESTOPBIT;
+    success = SetCommState(port, &dcb);
+    if (!success)
+    {
+        printf("Failed to set serial settings");
+        CloseHandle(port);
+        return INVALID_HANDLE_VALUE;
+    }
+
+    // display information
+    printf("----------------------------------\n");
+    printf("baud rate = %d\n", dcb.BaudRate);
+    printf("Parity = %d\n", dcb.Parity);
+    printf("Byte Size = %d\n", dcb.ByteSize);
+    printf("Stop Bit = %d\n", dcb.StopBits);
+    printf("----------------------------------\n");
+    return port;
+}
+
+int uart_transmit(HANDLE port, unsigned char* TX_buf, unsigned int TX_buf_len) {
+    int writtenbytes = 0;
+    if (WriteFile(port, TX_buf, TX_buf_len, (LPDWORD)&writtenbytes, NULL)) {
+        if (writtenbytes == 0) {
+            printf("WriteFile() timed out\n");
+            return -1;
+        }
+    }
+    else {
+        printf("WriteFile() failed\n");
+        return -1;
+    }
+    printf("%d bytes were written\n", writtenbytes);
+    return 0;
+}
+
+int uart_receive(HANDLE port, int* RX_buf_len, unsigned char* RX_buf) {
+    char readbuf;
+    int nbbytes = 0;
+
+    if (ReadFile(port, &readbuf, 1, (LPDWORD)&nbbytes, NULL)) {
+        if (nbbytes == 0) {
+            printf("ReadFile() timed out\n");
+            return -1;
+        }
+    }
+    else {
+        printf("ReadFile() failed\n");
+        return -1;
+    }
+    *RX_buf_len = 1;
+    RX_buf[0] = readbuf;
+
+    return 0;
+}
+
+
+int main()
+{
+    // configuration parameters
+    const char* device = "\\\\.\\COM7";
+    unsigned long baud_rate = 9600;
+    unsigned char bit_size = 8;
+    unsigned char parity = 0;
+    // code
+    int status = -1;
+    unsigned char TX_buf[BUFFER_SIZE], RX_buf[BUFFER_SIZE];
+    unsigned int TX_buf_len = 0, RX_buf_len = 0;
+
+    HANDLE port = open_port(device, baud_rate, bit_size, parity);
+    if (port == INVALID_HANDLE_VALUE) {
+        return -1;
+    }
+
+    TX_buf_len = 1;
+    printf("Enter 1-byte data (HEX) for sending:\n");
+    scanf("%x", &TX_buf[0]);
+
+    // transmit data
+    status = uart_transmit(port, TX_buf, TX_buf_len);
+    if (status != 0)
+        return -1;
+
+    // Receive data
+    status = uart_receive(port, &RX_buf_len, RX_buf);
+    if (status != 0)
+        return -1;
+
+    // Display 
+    printf("Received packet: ");
+    for (int j = 0; j < RX_buf_len; j++) {
+        printf("%x\t", RX_buf[j]);
+    }
+
+    // Close the serial port.
+    if (!CloseHandle(port))
+    {
+        printf("CloseHandle() failed\n");
+        return -1;
+    }
+    return 0;
+
+}
---
+++ UART/UART_SILABS/main_demo_2.c
@@ -0,0 +1,155 @@
+/***************************************************************************//**
+ * @file main.c
+ * @brief main() function.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "sl_component_catalog.h"
+#include "sl_system_init.h"
+#include "app.h"
+#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+#include "sl_power_manager.h"
+#endif
+#if defined(SL_CATALOG_KERNEL_PRESENT)
+#include "sl_system_kernel.h"
+#else // SL_CATALOG_KERNEL_PRESENT
+#include "sl_system_process_action.h"
+#endif // SL_CATALOG_KERNEL_PRESENT
+
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_usart.h"
+
+#define BSP_TXPORT gpioPortA
+#define BSP_RXPORT gpioPortA
+#define BSP_TXPIN 5
+#define BSP_RXPIN 6
+#define BSP_ENABLE_PORT gpioPortD
+#define BSP_ENABLE_PIN 4
+
+/**************************************************************************//**
+ * @brief
+ *    GPIO initialization
+ *****************************************************************************/
+void initGPIO(void)
+{
+  // Configure the USART TX pin to the board controller as an output
+  GPIO_PinModeSet(BSP_TXPORT, BSP_TXPIN, gpioModePushPull, 1);
+
+  // Configure the USART RX pin to the board controller as an input
+  GPIO_PinModeSet(BSP_RXPORT, BSP_RXPIN, gpioModeInput, 0);
+
+  /*
+   * Configure the BCC_ENABLE pin as output and set high.  This enables
+   * the virtual COM port (VCOM) connection to the board controller and
+   * permits serial port traffic over the debug connection to the host
+   * PC.
+   *
+   * To disable the VCOM connection and use the pins on the kit
+   * expansion (EXP) header, comment out the following line.
+   */
+  GPIO_PinModeSet(BSP_ENABLE_PORT, BSP_ENABLE_PIN, gpioModePushPull, 1);
+}
+
+/**************************************************************************//**
+ * @brief
+ *    USART0 initialization
+ *****************************************************************************/
+void initUSART0(void)
+{
+  // Default asynchronous initializer (115.2 Kbps, 8N1, no flow control)
+  //USART_InitAsync_TypeDef init = USART_INITASYNC_DEFAULT;
+  USART_InitAsync_TypeDef init;
+
+  init.enable = usartEnable;
+  init.refFreq = 0;
+  init.baudrate = 115200;
+  init.oversampling = usartOVS16;
+  init.databits = usartDatabits8;
+  init.parity = USART_FRAME_PARITY_NONE;
+  init.stopbits = usartStopbits1;
+
+  init.mvdis = false;
+  init.prsRxEnable = false;
+  init.prsRxCh = 0;
+
+  init.autoCsEnable = false;
+  init.csInv = false;
+  init.autoCsHold = 0;
+  init.autoCsSetup = 0;
+  init.hwFlowControl = usartHwFlowControlNone;
+
+  // Route USART0 TX and RX to the board controller TX and RX pins
+  GPIO->USARTROUTE[0].TXROUTE = (BSP_TXPORT << _GPIO_USART_TXROUTE_PORT_SHIFT)
+            | (BSP_TXPIN << _GPIO_USART_TXROUTE_PIN_SHIFT);
+  GPIO->USARTROUTE[0].RXROUTE = (BSP_RXPORT << _GPIO_USART_RXROUTE_PORT_SHIFT)
+            | (BSP_RXPIN << _GPIO_USART_RXROUTE_PIN_SHIFT);
+
+  // Enable RX and TX signals now that they have been routed
+  GPIO->USARTROUTE[0].ROUTEEN = GPIO_USART_ROUTEEN_RXPEN | GPIO_USART_ROUTEEN_TXPEN;
+
+  // Configure and enable USART0
+  USART_InitAsync(USART0, &init);
+}
+
+int main(void)
+{
+  // Initialize Silicon Labs device, system, service(s) and protocol stack(s).
+  // Note that if the kernel is present, processing task(s) will be created by
+  // this call.
+  sl_system_init();
+
+  // Initialize the application. For example, create periodic timer(s) or
+  // task(s) if the kernel is present.
+  app_init();
+
+#if defined(SL_CATALOG_KERNEL_PRESENT)
+  // Start the kernel. Task(s) created in app_init() will start running.
+  sl_system_kernel_start();
+#else // SL_CATALOG_KERNEL_PRESENT
+
+  uint8_t buffer;
+
+  // Initialize GPIO and USART0
+  initGPIO();
+  initUSART0();
+
+  while (1) {
+    // Do not remove this call: Silicon Labs components process action routine
+    // must be called from the super loop.
+    sl_system_process_action();
+
+    // Application process.
+    app_process_action();
+
+  // Zero out buffer
+    buffer = 0;
+   // Receive BUFLEN characters unless a new line is received first
+   do
+   {
+     // Wait for a character
+     buffer = USART_Rx(USART0);
+   }
+   while ( buffer == 0 );
+
+   // Output received characters
+    USART_Tx(USART0, buffer);
+
+#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+    // Let the CPU go to sleep if the system allows it.
+    sl_power_manager_sleep();
+#endif
+  }
+#endif // SL_CATALOG_KERNEL_PRESENT
+}
